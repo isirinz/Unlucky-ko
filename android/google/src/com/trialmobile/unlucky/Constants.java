e1ed8fc379ce92479d8cfe7f5ce5464fe2ebd42e
isirinz <isirinz@naver.com>
2022-04-05 10:24:16 +0900

    애드몹 테스트 ID

--- android/google/src/com/trialmobile/unlucky/Constants.java
+++ android/google/src/com/trialmobile/unlucky/Constants.java
@@ -1,7 +1,7 @@
 package com.trialmobile.unlucky;
 
 public class Constants {
-    public static final String RESYP_MEDIA_CODE = "";
-    public static final String AD_UNIT_ID = "";
-    public static final String REWARDED_UNIT_ID = "";
+    public static final String RESYP_MEDIA_CODE = "55a94c2c-0000-0000-abf6-01fa66db2a01";
+    public static final String AD_UNIT_ID = "ca-app-pub-3940256099942544/6300978111";
+    public static final String REWARDED_UNIT_ID = "ca-app-pub-3940256099942544/5224354917";
 }
